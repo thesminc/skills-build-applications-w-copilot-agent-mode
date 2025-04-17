dfbea7224b3acbd8ee8700b02a3b33a9c0a5f245
Louis Barber <69613+thesminc@users.noreply.github.com>
2025-04-17 00:26:12 +0000

    Update populate_db.py to use test_data.py for database population

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_database.py
@@ -0,0 +1,35 @@
+from django.core.management.base import BaseCommand
+from octofit_tracker.models import User, Team, Activity, Leaderboard, Workout
+from octofit_tracker.test_data import get_test_data
+
+def populate_database():
+    test_data = get_test_data()
+
+    # Populate users
+    for user in test_data['users']:
+        User.objects.create(**user)
+
+    # Populate teams
+    for team in test_data['teams']:
+        Team.objects.create(**team)
+
+    # Populate activities
+    for activity in test_data['activities']:
+        user = User.objects.get(username=activity.pop('user'))
+        Activity.objects.create(user=user, **activity)
+
+    # Populate leaderboard
+    for entry in test_data['leaderboard']:
+        user = User.objects.get(username=entry.pop('user'))
+        Leaderboard.objects.create(user=user, **entry)
+
+    # Populate workouts
+    for workout in test_data['workouts']:
+        Workout.objects.create(**workout)
+
+class Command(BaseCommand):
+    help = 'Populate the octofit_db database with test data'
+
+    def handle(self, *args, **kwargs):
+        populate_database()
+        self.stdout.write(self.style.SUCCESS('Successfully populated the database with test data'))
