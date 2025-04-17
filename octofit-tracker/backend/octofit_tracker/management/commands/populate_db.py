7ddbcbee8fa4c7101eabcb8d2d59d8f9dd674a76
Louis Barber <69613+thesminc@users.noreply.github.com>
2025-04-17 00:28:00 +0000

    Update populate_db.py

--- octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 from octofit_tracker.models import User, Team, Activity, Leaderboard, Workout
 from octofit_tracker.test_data import get_test_data
 
-def populate_database():
+def populate_db():
     test_data = get_test_data()
 
     # Populate users
@@ -31,5 +31,5 @@ class Command(BaseCommand):
     help = 'Populate the octofit_db database with test data'
 
     def handle(self, *args, **kwargs):
-        populate_database()
+        populate_db()
         self.stdout.write(self.style.SUCCESS('Successfully populated the database with test data'))
